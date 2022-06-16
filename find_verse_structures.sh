46aa5fb40dda7fb596c873e044deb60eb44aa620
Sigfrid Lundberg <siggelundberg@gmail.com>
2022-06-16 13:50:15 +0200

    strophe structure

--- find_verse_structures.sh
+++ find_verse_structures.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Calculates the distribution of strophe structures. Presenting them
+# in a html table.
+#
+# ./find_verse_structures.sh > strophe_structure_distro.xml
+# 
+
 source "parameters.sh"
 
 XSL="$HERE/verse_structure.xsl"
