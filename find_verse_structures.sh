252a851394363be78d69f8295890a0c111f5a454
Sigfrid Lundberg <siggelundberg@gmail.com>
2022-06-16 13:22:46 +0200

    centralized parameters.sh sourced from all scripts

--- find_verse_structures.sh
+++ find_verse_structures.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-SAXON_JAR="/usr/share/maven-repo/net/sf/saxon/Saxon-HE/9.9.1.5/Saxon-HE-9.9.1.5.jar"
-SAXON="java -jar $SAXON_JAR"
-
-PROJECTS="$HOME/projects"
-
-HERE="$PROJECTS/danish-sonnets"
+source "parameters.sh"
 
 XSL="$HERE/verse_structure.xsl"
 
